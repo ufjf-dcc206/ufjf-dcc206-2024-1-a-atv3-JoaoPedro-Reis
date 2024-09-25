e31e43f2348108215ed8cfe429ca4fc9acd1117b
Joao Pedro Reis <joaoreis.pedro@estudante.ufjf.br>
2024-09-24 22:09:30 -0300

    cria funçoes fetch e buscaPoke

---
+++ main.ts
@@ -0,0 +1,12 @@
+const url = "https://pokeapi.co/api/v2/pokemon/";
+
+const buscaPoke = () => {
+    const nome = document.querySelector
+
+}
+
+const sendByFetch = async (pokeName) => {
+    try {
+        
+    }
+}
